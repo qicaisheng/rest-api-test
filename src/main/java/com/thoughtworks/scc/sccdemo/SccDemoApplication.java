1e33d84d4f468b9b92510599ba5d84abcda90d42
qicaisheng <qicaisheng@qq.com>
2020-04-14 21:18:23 +0800

    init commit

---
+++ src/main/java/com/thoughtworks/scc/sccdemo/SccDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.scc.sccdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SccDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/scc/sccdemo/SccDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.scc.sccdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
