d6bbe976cce01adb5e3d18a1a25cabeb3f8a1f20
qicaisheng <qicaisheng@qq.com>
2020-04-15 18:14:38 +0800

    format factory

--- src/main/java/com/thoughtworks/scc/sccdemo/CompanyFactory.java
+++ src/main/java/com/thoughtworks/scc/sccdemo/CompanyFactory.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class CompanyFactory {
     public static List<Company> getTestCompany() {
         List<Employee> employees=new ArrayList<>();
+        
         employees. add(new Employee(1,"小明",10,"male",6000));
         employees. add(new Employee(2,"小红",20,"female",6000));
         Company company=new Company(1,"thoughtworks",employees.size(),employees);
+        
         List<Company> companies=new ArrayList<>();
         companies.add(company);
         companies.add(company);
@@ -17,6 +19,7 @@ public class CompanyFactory {
         companies.add(company);
         companies.add(company);
         companies.add(company);
+        
         return companies;
     }
 }
--- src/main/java/com/thoughtworks/scc/sccdemo/EmployeeFactory.java
+++ src/main/java/com/thoughtworks/scc/sccdemo/EmployeeFactory.java
@@ -8,6 +8,7 @@ public class EmployeeFactory {
         List<Employee> employees=new ArrayList<>();
         employees.add(new Employee(1,"xiaoming",22,"male",5000));
         employees.add(new Employee(2,"xiaohong",24,"female",6000));
+        
         return employees;
     }
 }
